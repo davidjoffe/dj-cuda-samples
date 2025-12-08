0ad3dfe7e5bcf8a68e2c3132a46888e78b6f8536
David Joffe <david@djoffe.com>
2025-12-08 21:39:03 +0200

    Build script improvements - allow args passed

--- build.bat
+++ build.bat
@@ -1,7 +1,18 @@
 @rem dj CUDA samples
 @rem Build script for Windows using vcpkg and cmake
 @rem Adjust the VCPKG variable to point to your vcpkg installation
-@rem Usage: build.bat
+@rem Usage: build.bat (optional-args-to-pass-to-built-app)
+
+@rem Optionally you may pass in run-time arguments to the built application here, though this is not required:
+
+@rem Examples:
+
+@rem Start paused
+@rem build.bat --paused
+@rem or with number of items/entities/particles to simulate:
+@rem build.bat --paused -N 50000
+@rem or for headless mode test, or for benchmark or test runs:
+@rem build.bat -N 1000000 --headless --maxframes 10000
 
 @rem https://github.com/davidjoffe/dj-cuda-samples
 @rem Copyright David Joffe 2025
@@ -14,8 +25,10 @@ set VCPKG=C:\v
 rem clean
 rd /s /q build
 
-cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=%VCPKG%/scripts/buildsystems/vcpkg.cmake
-cmake --build build
+cmake -S . -B build-windows -DCMAKE_TOOLCHAIN_FILE=%VCPKG%/scripts/buildsystems/vcpkg.cmake
+cmake --build build-windows
 
-@rem run the built demo
-.\build\bouncing-balls\Debug\djbouncing_balls_demo.exe"
+@rem run the built application
+@rem Just pass in passed-in command line args to this script, that allows us to call build with different args to pass to the auto run here:
+@echo dj:build-windows: Run djbouncing_balls_demo.exe %*
+".\build-windows\bouncing-balls\Debug\djbouncing_balls_demo.exe" %*
--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 # dj-cuda-samples
 # Build helper script: Linux, WSL etc.
+#
+# Usage: ./build.sh
+#
+# Optionally you may also pass in arguments to this build script which will be simply passed on to the built app when it is run.
+# Examples:
+# ./build.sh -N 50000
+# ./build.sh --paused
+# ./build.sh -N 1000000 --headless --maxframes 1000
 
 # Clean previous build if any
 rm -rf build-linux
@@ -10,4 +18,7 @@ cmake -S . -B build-linux
 cmake --build build-linux
 
 # Run
-./build-linux/bouncing-balls/djbouncing_balls_demo
+# Just pass in passed-in command line args to this script, that allows us to call build with different args to pass to the auto run here:
+#./build-linux/bouncing-balls/djbouncing_balls_demo --paused -N 200000  --headless --maxframes 10000
+@echo dj:build-linux: Run app $@
+./build-linux/bouncing-balls/djbouncing_balls_demo $@
